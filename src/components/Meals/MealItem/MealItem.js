eb9b63ad73b3161eb427c30e813b2af6c73d0f10
Dmitriy <dimazurenko@gmail.com>
2022-12-26 14:16:47 +0300

    add changed Card amount

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,24 @@
+import { useContext } from 'react';
+
 import MealItemForm from './MealItemForm';
+
+import CartContext from '../../../store/cart-context';
 
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCardHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -13,7 +28,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCardHandler} />
       </div>
     </li>
   );
